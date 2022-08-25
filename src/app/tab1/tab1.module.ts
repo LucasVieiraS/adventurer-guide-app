7cba50096b80090445c8f0e1db1e1fb000f07b1b
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-08-25 08:21:48 -0300

    new: tab1

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { SwiperModule } from 'swiper/angular';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -13,7 +14,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    SwiperModule
   ],
   declarations: [Tab1Page]
 })
