d063367eb0b15a45b84a8e4868b33022c658269a
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-08-29 19:28:05 -0300

    changed: app name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1>TightHope</h1>
+<h1>AdventurerGuide</h1>
 <p>Ionic Application that assists in daily tasks of adventurous people.</p>
 
 <img src="src/assets/app-icon.png" alt="drawing" width="200"/>
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'tightHope',
+  appName: 'AdventurerGuide',
   webDir: 'www',
   bundledWebRuntime: false
 };
